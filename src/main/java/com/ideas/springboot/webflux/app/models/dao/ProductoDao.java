a17201d4dd3130c8c8d3fb7047dffaa31b377879
israel <israel.bejarano.a@gmail.com>
2019-08-11 15:53:58 +0200

    producto dao

---
+++ src/main/java/com/ideas/springboot/webflux/app/models/dao/ProductoDao.java
@@ -0,0 +1,9 @@
+package com.ideas.springboot.webflux.app.models.dao;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.ideas.springboot.webflux.app.models.documents.Producto;
+
+public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
+
+}
