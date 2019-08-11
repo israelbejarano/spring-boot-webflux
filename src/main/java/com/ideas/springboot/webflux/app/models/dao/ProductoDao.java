59673cdce73d5a9e90780d5ee869ea10b24c8c92
israel <israel.bejarano.a@gmail.com>
2019-08-11 16:12:31 +0200

    add javadoc

--- src/main/java/com/ideas/springboot/webflux/app/models/dao/ProductoDao.java
+++ src/main/java/com/ideas/springboot/webflux/app/models/dao/ProductoDao.java
@@ -4,6 +4,10 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.ideas.springboot.webflux.app.models.documents.Producto;
 
+/**
+ * The Interface ProductoDao.
+ * @author Israel Bejarano
+ */
 public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
 
 }
