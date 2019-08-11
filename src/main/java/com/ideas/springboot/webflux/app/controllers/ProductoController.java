afb691372a1c04bc0df25ceab20e9b236a3fcdab
israel <israel.bejarano.a@gmail.com>
2019-08-11 16:27:05 +0200

    listar y listarDataDriver

--- src/main/java/com/ideas/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/ideas/springboot/webflux/app/controllers/ProductoController.java
@@ -1,9 +1,14 @@
 package com.ideas.springboot.webflux.app.controllers;
 
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.ideas.springboot.webflux.app.models.dao.ProductoDao;
 import com.ideas.springboot.webflux.app.models.documents.Producto;
@@ -16,12 +21,34 @@ public class ProductoController {
 	@Autowired
 	private ProductoDao dao;
 	
+	private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+	
 	@GetMapping({"/listar", "/"})
 	public String listar(Model model) {
-		Flux<Producto> productos = dao.findAll();
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		});
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
 		
 		model.addAttribute("titulo", "Listado de productos");
 		model.addAttribute("productos", productos);
+		return "listar";
+		
+	}
+
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		
+		model.addAttribute("titulo", "Listado de productos");
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 2));
 		return "listar";
 		
 	}
